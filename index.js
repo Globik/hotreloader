9bbf0bda89f679480946791f65057c10129cf1ba
Globik <gru5@yandex.ru>
2017-06-16 09:01:43 -0400

    removed module-invalidate-2

--- index.js
+++ index.js
@@ -4,16 +4,45 @@ const debug = require('debug')('hotreloader');
 const chokidar=require('chokidar');
 const path=require('path');
 const watch = new chokidar.FSWatcher({ ignored: /[\/\\]\./, persistent: true,awaitWriteFinish:{stabilityThreshold:2000,pollInterval:100} });
-require('module-invalidate-2');
-function reload(n){
-let l=path.resolve(n)
-watch.add(l);
-return require(l);
+
+function reload(required) {
+var modulePath = path.resolve(required);
+watch.add(modulePath);
+load(modulePath);
+return methods[modulePath];
 }
-watch.on('change', p => {
-debug('On change: ', p)
-module.invalidateByPath(p)			 
-					 })
+function swap(modulePath) {
+  var moduleCache = require.cache[modulePath];
+  var exportKeys = Object.keys(moduleCache.exports);
+
+  for (var i = 0; i < exportKeys.length; i++) {
+    var exportKey = exportKeys[i];
+    moduleCache.exports[exportKey] = null
+    delete moduleCache.exports[exportKey];
+  }
+
+  var tmpModulePath = modulePath + '.tmp';
+  require.cache[tmpModulePath] = require.cache[modulePath];
+  delete require.cache[modulePath];
+
+  load(modulePath);
+  require.cache[modulePath] = require.cache[tmpModulePath];
+  delete require.cache[tmpModulePath];
+
+  var methodKeys = Object.keys(methods[modulePath]);
+
+  for (var i = 0; i < methodKeys.length; i++) {
+    var methodKey = methodKeys[i];
+    moduleCache.exports[methodKey] = methods[modulePath][methodKey];
+  }
+}
+
+function load(modulePath) {
+  methods[modulePath] = require(modulePath);
+}
+
+watch.on('change', swap);
+
 module.exports = reload;
 watch.on('add', p => {
 debug('On added: ', p)
